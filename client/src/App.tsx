d9facf0d53e7470e35a1633ec8ac07b93fcb6249
kocohira <83955083+kocohira@users.noreply.github.com>
2025-03-22 00:17:08 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,8 +11,8 @@ import { HistoricalEvents } from "./historical-events.tsx";
 import { useDojoSDK, useModel } from "@dojoengine/sdk/react";
 
 import { React } from 'react';
-import { Chat } from './components/Chat.tsx';
-import { useWebSocket } from './hooks/WebSocket.tsx';
+import Chat from './components/Chat.tsx';
+import useWebSocket from './hooks/WebSocket.tsx';
 
 /**
  * Main application component that provides game functionality and UI.
