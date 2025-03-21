aa5d971faf7f96eb12fc705229249afb887167f8
kocohira <83955083+kocohira@users.noreply.github.com>
2025-03-21 22:30:36 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -263,7 +263,7 @@ function App() {
         </div>
     );
 
-    useWebSocket("ws://156.231.111.65:3001");
+    useWebSocket("wss://court-ill-bathrooms-subsection.trycloudflare.com");
       return (
         <div className="app">
           <h1>Chat Application</h1>
