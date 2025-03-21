0f70c9a77790eb2eeb3c6cfcf592800d274e1efe
kocohira <83955083+kocohira@users.noreply.github.com>
2025-03-21 19:38:09 +0800

    Update WebSocket.ts

--- client/src/hooks/WebSocket.ts
+++ client/src/hooks/WebSocket.ts
@@ -1,16 +1,29 @@
 import { useEffect } from "react";
-import { useChatStore } from "../store/useChatStore";
+import { useChatStore } from "../store/ChatStore";
 
-export const useWebSocket = () => {
+const useWebSocket = (url: string) => {
+  const addMessage = useChatStore(state => state.addMessage);
+
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:3001"); // here change to WebSocket server address.(local)
+    const ws = new WebSocket("ws://localhost:3001"); // here change to real WebSocket server address.
+
+    ws.onopen = () => {
+      console.log('WebSocket Connected');
+    };
 
     ws.onmessage = (event) => {
-      useChatStore.getState().receiveMessage(event.data);
+      const message = JSON.parse(event.data);
+      addMessage(message); // update message to store
+    };
+
+    ws.onerror = (error) => {
+      console.error('WebSocket error:', error);
     };
 
     return () => {
       ws.close();
     };
-  }, []);
+  }, [url, addMessage]);
 };
+
+export default useWebSocket;
